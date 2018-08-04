fda077b64cbc6801f157e8ed9cd02b61de023461
Ian Metcalf <ian@metcalfbuilt.com>
2018-08-04 15:05:41 -0400

    Added leveldb stream node

--- storage/leveldb/67-leveldb.js
+++ storage/leveldb/67-leveldb.js
@@ -52,6 +52,63 @@ module.exports = function(RED) {
     RED.nodes.registerType("leveldb in",LevelDBNodeIn);
 
 
+    function LevelDBNodeStream(n) {
+        RED.nodes.createNode(this,n);
+        this.level = n.level;
+        this.levelConfig = RED.nodes.getNode(this.level);
+
+        var node = this;
+        node.on("input", function(msg) {
+            if (node.levelConfig && node.levelConfig.ready) {
+                var options = msg.query || {};
+                delete msg.query;
+
+                msg.parts = {
+                    type: 'array',
+                    id: RED.util.generateId(),
+                    index: -1,
+                };
+
+                node.levelConfig.db.createReadStream(options)
+
+                .on('data', function(data) {
+                    if (msg.complete) return;
+
+                    if (msg.parts.index !== -1) {
+                        node.send(RED.util.cloneMessage(msg));
+                    }
+
+                    if (options.keys || options.values) {
+                        msg.payload = data;
+                    }
+                    else {
+                        msg.topic = data.key;
+                        msg.payload = data.value;
+                    }
+
+                    msg.parts.index += 1;
+                })
+
+                .on('end', function() {
+                    if (msg.complete) return;
+
+                    msg.complete = true;
+
+                    if (msg.parts.index !== -1) {
+                        node.send(RED.util.cloneMessage(msg));
+                    }
+                })
+
+                .on('error', function(err) {
+                    node.error(err);
+                });
+            }
+            else { node.error("Database not ready",msg); }
+        });
+    }
+    RED.nodes.registerType("leveldb stream",LevelDBNodeStream);
+
+
     function LevelDBNodeOut(n) {
         RED.nodes.createNode(this,n);
         this.level = n.level;
